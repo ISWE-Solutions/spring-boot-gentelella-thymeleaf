2c1f9a72976edef2f689bf7d454ac3ed3dfd809e
Hendi Santika <hendisantika@gmail.com>
2018-09-10 06:08:21 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootgentelellathymeleaf/SpringBootGentelellaThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootgentelellathymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGentelellaThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGentelellaThymeleafApplication.class, args);
+    }
+}
