928300a479a352a24022113825ed28d0259313f3
Hendi Santika <hendisantika@gmail.com>
2018-09-10 06:14:00 +0700

    #4 - Add Form Controller

--- src/main/java/com/hendisantika/springbootgentelellathymeleaf/controller/FormController.java
+++ src/main/java/com/hendisantika/springbootgentelellathymeleaf/controller/FormController.java
@@ -10,29 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Email: hendisantika@gmail.com
  * Telegram : @hendisantika34
  * Date: 10/09/18
- * Time: 06.11
+ * Time: 06.13
  * To change this template use File | Settings | File Templates.
  */
 
 @Controller
-public class IndexController {
-    @GetMapping("/")
-    public String index(){
-        return "index1";
+public class FormController {
+    @GetMapping("/form")
+    public String form() {
+        return "form";
     }
 
-    @GetMapping("/index1")
-    public String index1(){
-        return "index1";
-    }
-
-    @GetMapping("/index2")
-    public String index2(){
-        return "index2";
-    }
-
-    @GetMapping("/index3")
-    public String index3(){
-        return "index3";
+    @GetMapping("/form_advanced")
+    public String formAdvanced() {
+        return "form_advanced";
     }
 }
